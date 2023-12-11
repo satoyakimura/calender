b5e9774abe8f7b8be7af026e7a4aa56906d17f05
satoyakimura <k.satoya37@gmail.com>
2023-12-12 03:13:24 +0900

    おやすみ2

--- app/api/api.py
+++ app/api/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1 import router as v1_router
+from app.api.v1 import v1_router
 
 api_router = APIRouter()
 api_router.include_router(v1_router, prefix="/v1")
