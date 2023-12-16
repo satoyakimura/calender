f956962ca3484a70461fdf6c8ba30e61cb081dd9
satoyakimura <k.satoya37@gmail.com>
2023-12-16 21:14:12 +0900

    12-16 なんもしてないコミット

--- app/api/v1/endpoints/schedule.py
+++ app/api/v1/endpoints/schedule.py
@@ -1,5 +1,7 @@
-# from fastapi import FastAPI
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+schedule_router = FastAPI()
 
-# schedule_router = FastAPI()
-
-# # @schedule_router.get()
+@schedule_router.get("/schedule", response_class=HTMLResponse)
+async def schedule(request: Request):
+    return templates.TemplateResponse()
